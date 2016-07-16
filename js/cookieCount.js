acbe9b72942451b6a0ebbfe14bb0fac763c70cd5
Waxhoya <waxhoya@gmail.com>
2016-07-16 04:30:08 -0700

    created cookieStand(...)

--- js/cookieCount.js
+++ js/cookieCount.js
@@ -1,18 +1,25 @@
-var salmonSteve = {
-	minCst: 1,
-	maxCst: 10,
-	pricePer: 1,
-	this.getHour {
-		return cookieCount(minCst, maxCst, pricePer);
-	}
 
-}
-
-function cookieCount(minCst, maxCst, pricePer) {
-
-
-}
 //Taken from MDN
 function randInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+function cookieStand(minCst, maxCst, avgPer, ) {
+	this.minCst = minCst;
+	this.maxCst = maxCst;
+	this.avgPer = avgPer;
+	var lastProfit = "";
+	function getCustomers() {
+		lastProfit = randInt(minCst, maxCst);
+		return lastSold
+	}
+	function getSales() {
+		return lastSold*this.avgPer
+	}
+}
+
+var pioneerSquare = new cookieStand(17, 88, 5.2);
+var portlandAirport = new cookieStand(6, 24, 1.2);
+var Washington Square = new cookieStand(11, 38, 1.9);
+var Sellwood = new cookieStand(20, 48, 3.3);
+var Pearl Distric = new cookieStand(3, 24, 2.6);
