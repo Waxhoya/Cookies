13781eeb2f075cc9bf7c7f83cebe0a71c46f2cde
Waxhoya <waxhoya@gmail.com>
2016-07-16 11:38:19 -0700

    Adding name to cookieStand Obj

--- js/cookieCount.js
+++ js/cookieCount.js
@@ -3,7 +3,8 @@ function randInt(min, max) {
 	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function cookieStand(minCst, maxCst, avgPer) {
+function cookieStand(name, minCst, maxCst, avgPer) {
+	this.name = name:
 	this.minCst = minCst;
 	this.maxCst = maxCst;
 	this.avgPer = avgPer;
@@ -17,16 +18,17 @@ function cookieStand(minCst, maxCst, avgPer) {
 	}
 }
 
-
+//
 var storeArray = [];
 
-var pioneerSquare = new cookieStand(17, 88, 5.2);
+//Populating array with known stores
+var pioneerSquare = new cookieStand("Pioneer Square" 17, 88, 5.2);
 storeArray.push('pioneerSquare');
-var portlandAirport = new cookieStand(6, 24, 1.2);
+var portlandAirport = new cookieStand("Portland Airport", 6, 24, 1.2);
 storeArray.push('portlandAirport');
-var washingtonSquare = new cookieStand(11, 38, 1.9);
+var washingtonSquare = new cookieStand("Washington Square", 11, 38, 1.9);
 storeArray.push('washingtonSquare');
-var sellwood = new cookieStand(20, 48, 3.3);
+var sellwood = new cookieStand("Sellwood", 20, 48, 3.3);
 storeArray.push('sellwood');
-var pearlDistric = new cookieStand(3, 24, 2.6);
+var pearlDistric = new cookieStand("Pearl District", 3, 24, 2.6);
 storeArray.push('pearlDistric');
