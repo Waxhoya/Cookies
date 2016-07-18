fc90bf15100ab070d4922b0710709918a3f7607e
Waxhoya <waxhoya@gmail.com>
2016-07-17 18:53:06 -0700

    rewrite of html output

--- js/cookieCount.js
+++ js/cookieCount.js
@@ -1,3 +1,9 @@
+// takes index int and returns hour strings
+function whatTime(index) {
+	if (index > 12) {return index+":00"}
+	else { return (index-12)+":00"}
+}
+
 // Returns a random number between min and max
 function randInt(min, max) {
 	return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -37,19 +43,24 @@ var pearlDistrict = new cookieStand("Pearl District", 3, 24, 2.6);
 storeArray.push(pearlDistrict);
 
 // iterate through all of the available cookieStand
-function howIsDay() {
-	for (var currentStore = 0; currentStore > storeArray.length; currentStore++) {
-		var dayTotal, dailyList;
-		dayTotal = 0;
-		dailyList = "<ul>";
-		console.log("loop 1"+dayTotal+" "+dailyList)
-		for (var hours = 0; hours > 7; hours++) {
-			console.log("loop 2"+dayTotal+" "+dailyList)
-			var hourlyOut = storeArray[currentStore].getSales();
-			dayTotal += hourlyOut;
-			dailyList += "<li>"+ hourlyOut +"</li>";
+function howIsDay(arrayOfStores) {
+	for (var currentStore = 0; currentStore < arrayOfStores.length; currentStore++) {
+		var howHour, dailyCookies, dailyCust; //howHour, track each hour,  dailyCookies tracks total a day
+		dailyCookies =0;
+		dailyCust =0;
+
+		document.write("<table><tr><th scope='col'>Customers</th><th scope='col'>Cookies Sold</th><th scope='col'>Time Sold</th></tr>");
+
+		for (var hours = 0; hours < 7; hours++) {
+			howHour = arrayOfStores[currentStore].getCustomers()
+			qtyCookie = arrayOfStores[currentStore].getSales()
+			document.write("<tr><td>"+howHour+"</td><td>"+qtyCookie+"</td><td>" +whatTime(hours)+ "</td></tr>")
+			dailyCust += howHour;
+			dailyCookies += qtyCookie;
 		}
-		dailyList += "<li class='dayTotal'> Total: "+dayTotal+"</li></ul>";
-		document.getElementById("cookieTotals") = dailyList;
+		document.write("<tr><td>DAILY TOTAL</td><td>TOTAL COOKIES</td><td>TOTAL CUSTOMERS</td></tr>");
+		document.write("<tr><td>"+dailyCust"</td><td>"+dailyCookies+"</td><td>"+dailyCookies+"</td></tr>");
+
 	}
+
 }
