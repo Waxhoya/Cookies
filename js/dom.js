bfc3fc8bb6c0370ac9eb40a9699a863b55589189
Waxhoya <waxhoya@gmail.com>
2016-07-20 20:26:16 -0700

    cleaning up CookieStore into its own JS file

--- js/dom.js
+++ js/dom.js
@@ -8,10 +8,10 @@ function dayTable(arrayOfStores) {
 		dailyCookies =0;
 		dailyCust =0;
 
-
 		for (var hours = 10; hours < 18; hours++) {
 
 			newRow = table.insertRow(table.rows.length);
+			console.log("creating row: "+table.rows.length);
 			var cell1 = newRow.insertCell(0);
 			var cell2 = newRow.insertCell(1);
 			var cell3 = newRow.insertCell(2);
