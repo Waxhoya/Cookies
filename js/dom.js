f444b2e28c7b4933a245de9d0640845f380fcd66
Waxhoya <waxhoya@gmail.com>
2016-07-20 20:14:30 -0700

    Sarah Joy suggested bugfixes. Output of dom.js improved

--- js/dom.js
+++ js/dom.js
@@ -1,17 +1,17 @@
-
-
 function dayTable(arrayOfStores) {
+	// var newTable = document.createElement("TABLE");
 
 	for (var currentStore = 0; currentStore < arrayOfStores.length; currentStore++) {
+		var table = document.getElementById("cookieDOM");
+
 		var howHour, dailyCookies, dailyCust; //howHour track each hour,  dailyCookies tracks total a day
 		dailyCookies =0;
 		dailyCust =0;
 
-		for (var hours = 10; hours < 18; hours++) {
-			var table = document.getElementById("cookieDOM");
-			var newTable = document.createElement("TABLE");
 
-			var newRow = table.insertRow(currentStore);
+		for (var hours = 10; hours < 18; hours++) {
+
+			newRow = table.insertRow(table.rows.length);
 			var cell1 = newRow.insertCell(0);
 			var cell2 = newRow.insertCell(1);
 			var cell3 = newRow.insertCell(2);
